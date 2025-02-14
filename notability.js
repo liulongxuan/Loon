8b632428969ff1fff2ea9f17b56943afd2670ee8
liulongxuan <115394664+liulongxuan@users.noreply.github.com>
2025-02-14 09:50:38 +0800

    更新 notability.js

--- notability.js
+++ notability.js
@@ -9,7 +9,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://github.com/liulongxuan/Loon/blob/main/notability.js
+^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://raw.githubusercontent.com/liulongxuan/Loon/blob/main/notability.js
 
 [mitm]
 hostname = notability.com
